d4a324e8d67e919cb0fcab8748d7f4bc01b9a547
shailajadurgam60-ai <shailajadurgam60@gmail.com>
2026-08-01 17:25:49 +0530

    Fix the API_KEY to supprort Render

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
-import requests
+import os
 import streamlit as st
 
-API_KEY = st.secrets["OPENWEATHER_API_KEY"]
+API_KEY = st.secrets.get("OPENWEATHER_API_KEY") or os.getenv("OPENWEATHER_API_KEY")
 @st.cache_data(ttl=86400)
 def get_coordinates(city):
     url = (
